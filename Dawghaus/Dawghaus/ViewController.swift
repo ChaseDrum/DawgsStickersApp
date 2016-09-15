e6eaa5bd571ef981367d7ecd306dbda5b89d2eba
Chase Drum <ChaseDrum@Chases-2015-MacBook-Pro.local>
2016-09-14 18:20:33 -0700

    Version1.0 submitted to App Store.

---
+++ Dawghaus/Dawghaus/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Dawghaus
+//
+//  Created by Chase Drum on 9/6/16.
+//  Copyright © 2016 Chase Drum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var signInTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+/*
+    //From URL: https://developers.facebook.com/docs/facebook-login/ios#loginkit
+    // Add this to the header of your file, e.g. in ViewController.m
+    // after #import "ViewController.h"
+    #import <FBSDKCoreKit/FBSDKCoreKit.h>
+    #import <FBSDKLoginKit/FBSDKLoginKit.h>
+    
+    // Add this to the body
+    @implementation ViewController
+    
+    - (void)viewDidLoad {
+    [super viewDidLoad];
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    // Optional: Place the button in the center of your view.
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
+    }
+    
+    @end
+  */
+    
+    
+   
+}
+
